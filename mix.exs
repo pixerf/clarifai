c8c485cdac40fa0052e0df8b130723d7823fe608
Charles C. Lee <charleschanlee@gmail.com>
2017-07-23 15:36:20 -0700

    update to 0.2.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Clarifai.Mixfile do
 
   def project do
     [app: :clarifai,
-     version: "0.1.1",
+     version: "0.2.1",
      elixir: "~> 1.4",
      preferred_cli_env: [espec: :test],
      build_embedded: Mix.env == :prod,
